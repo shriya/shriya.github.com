0189c9f4a74cff8c5d41551d257d0ee267943aa8
Shriya K Nevatia <sneva831@gmail.com>
2014-03-14 16:17:00 -0400

    Found it I think

--- transit/map.js
+++ transit/map.js
@@ -404,7 +404,7 @@ function renderMap()
         
 //  service = new google.maps.places.PlacesService(map);
 // service.search(request, callback);
-//}
+}
       
 // Taken from http://code.google.com/apis/maps/documentation/javascript/places.html
 function callback(results, status)
